1a67cd1ff9045fc358ecf559930e56f53bcdb5ba
VladiqDanilov <90576351+VladiqDanilov@users.noreply.github.com>
2023-09-15 21:48:38 +0300

    changed the encoding of cpp file

--- Control Task2.2/Control proj 2.2.cpp
+++ Control Task2.2/Control proj 2.2.cpp
@@ -2,56 +2,78 @@
 //
 
 #include <iostream>
+#include <string>
 using namespace std;
+
+bool has_no_more_than_2_consec_chars(const std::string& input_str) {
+    int conseс_cnt = 1;
+
+    for (int i = 1; i < input_str.length(); ++i) {
+        if (input_str[i] == input_str[i - 1]) {
+            conseс_cnt++;
+            if (conseс_cnt > 2) {
+                return false;
+            }
+        }
+        else {
+            conseс_cnt = 1;
+        }
+    }
+
+    return true;
+}
 
 int main()
 {
     setlocale(LC_ALL, "ru");
-    int n;
-    int s;
-    int ch=0;
+    string inpt1;
+    //int s;
+    string ch;
     bool validn = false;
 
-    cout << " введите номер СНИЛС содержащий 9 символов"<<endl;
-    cin >> n;
-    cout << "\n" << " введите Контрольное Число" << endl;
-    cin >> s;
+    cout << " введите номер и серию снисл длинной 11 символов"<<endl;
+    cin >> inpt1;
     cout << "\n";
+    string s = inpt1.substr(9, 11);
+    string str_n = inpt1.substr(0, 9);
+    bool no_more_than_2_consec_ch = has_no_more_than_2_consec_chars(str_n);
+    int n = stoi(str_n);
 
-    if (n >= 100000000 && n <= 999999999) {
+
+    if (str_n.length()==9 && no_more_than_2_consec_ch) {
         int sum = 0;
-        int pos = 9;
-        while (n>0)
+        int pos = 1;
+        while (n > 0)
         {
             int dig = n % 10;
             sum += dig * pos;
-            pos--;
+            pos++;
             n /= 10;
         }
         if (sum < 100) {
-            ch = sum;
+            ch = to_string(sum);
         }
         if (sum == 100 || sum == 101) {
-            ch = 00;
+            ch = '00';
         }
         if (sum > 101) {
             int ost = sum % 101;
             if (ost < 100) {
-                ch = ost;
+                ch = to_string(ost);
             }
             if (ost == 100) {
-                ch = 00;
+                ch = '00';
             }
         }
     }
     else {
-        cout << " номер СНИЛС должнен быть 9-значным.";
+        cout << " такого снилса не существует";
         exit;
     }
     if (ch == s) {
         validn = true;
     }
-    cout << "result: " << endl;
+    cout << "\n" << "result: " << endl;
     cout << "\n" << std::boolalpha << validn << std::endl;
 }
 
